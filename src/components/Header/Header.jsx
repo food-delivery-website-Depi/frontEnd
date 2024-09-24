5bb4bb153476907cf4a32ea8ab8528c4ab055641
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2024-09-24 20:47:06 +0300

    complate main design & make it responsive

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Get your favourite food delivered to your doorsteps Get your favourite
+          Get your favourite food delivered to your doorsteps food delivered to
+          your doorsteps Get your favourite food delivered to your doorsteps</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header
---
+++ src/pages/placeOrder/PlaceOrder.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './placeOrder.css'
+const PlaceOrder = () => {
+  return (
+    <div>PlaceOrder</div>
+  )
+}
+
+export default PlaceOrder
