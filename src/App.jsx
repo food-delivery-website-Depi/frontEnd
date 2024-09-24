7150144711016576ee9c3942367447c6c9c60331
SolyGh <125503517+SolyGh@users.noreply.github.com>
2024-09-25 00:06:56 +0300

    before merging sayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
@@ -13,19 +12,15 @@ const App = () => {
 
   return (
     <>
-      {/* Show Login Popup if showLogin is true */}
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
 
       <div className="app">
-        {/* Wrap the entire app in Router */}
-        <Router>
-          <Navbar setShowLogin={setShowLogin} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/order" element={<PlaceOrder />} />
-          </Routes>
-        </Router>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
       </div>
     </>
   );
--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,8 +1,47 @@
+import "./Cart.css";
+import { StoreContext } from "../../Context/StoreContext";
+import { useContext } from "react";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
-export default Cart
+  // Logging the cartItems and food_list for debugging
+  console.log("Cart Items:", cartItems);
+  console.log("Food List:", food_list);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          const quantity = cartItems[item._id];
+
+          if (quantity !== undefined && quantity > 0) {
+            return (
+              <div className="cart-item-title cart-items-item" key={index}>
+                <img src={item.image} alt={item.name} />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{quantity}</p>
+                <p>{item.price * quantity}</p>
+                <p onClick={() => removeFromCart(item._id)}>x</p>
+              </div>
+            );
+          }
+          return null; // Return null for items not in the cart
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
